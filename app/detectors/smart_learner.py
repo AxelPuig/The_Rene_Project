9eb87538017d6dfbe79dd3b219cbd739ecba5acc
Romain Milon <romain.milon@student-cs.fr>
2018-11-19 11:28:31 +0100

    Implemented smart learner to make database

--- app/detectors/smart_learner.py
+++ app/detectors/smart_learner.py
@@ -9,8 +9,6 @@ import time
 import sys
 import os
 
-database = []
-
 frame_process_size = [(192,108), (256,144), (320,180), (300,300), (426,240), (640,360), (1280,720)][3]
 face_process_size = [(48,48), (72,72), (96,96)][2]
 conf_threshold = .2
@@ -112,78 +110,8 @@ def serialize():
     f.write(pickle.dumps(le))
     f.close()
 
-def learn(image, data, debug=False):
-    global rectangles
-    database, recognizer, le = data
-
-    # resize the frame to have a width of 600 pixels (while
-    # maintaining the aspect ratio), and then grab the image
-    # dimensions
-    frame = imutils.resize(image, width=600)
-    (h, w) = frame.shape[:2]
-
-    # construct a blob from the image
-    image_blob = cv2.dnn.blobFromImage(cv2.resize(frame, frame_process_size), 1.0, frame_process_size, (104.0, 177.0, 123.0), swapRB=False, crop=False)
-
-    # apply OpenCV's deep learning-based face detector to localize
-    # faces in the input image
-    net.setInput(image_blob)
-    detections = net.forward()
-
-    new_rectangles = []
-
-    # loop over the detections
-    for i in range(0, detections.shape[2]):
-        # extract the confidence (i.e., probability) associated with
-        # the prediction
-        confidence = detections[0, 0, i, 2]
-
-        if confidence > conf_threshold:
-            # compute the (x, y)-coordinates of the bounding box for
-            # the face
-            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-            x1, y1, x2, y2 = box.astype("int")
-
-            name, proba, d = nearest_face((x1+x2)/2, (y1+y2)/2)
-            if d == -1 or d > 400 or frame_count % 10 == 0:
-                # extract the face ROI
-                face = frame[y1:y2, x1:x2]
-                fH, fW = face.shape[:2]
-
-                # ensure the face width and height are sufficiently large
-                if fW < 20 or fH < 20:
-                    continue
-
-                # construct a blob for the face ROI, then pass the blob
-                # through our face embedding model to obtain the 128-d
-                # quantification of the face
-                faceBlob = cv2.dnn.blobFromImage(face, 1.0 / 255, face_process_size, (0, 0, 0), swapRB=True, crop=False)
-                embedder.setInput(faceBlob)
-                vec = embedder.forward()
-
-                # perform classification to recognize the face
-                preds = recognizer.predict_proba(vec)[0]
-                j = np.argmax(preds)
-                proba = preds[j]
-                name = le.classes_[j]
-
-            # draw the bounding box of the face along with the
-            # associated probability
-            text = "{}: {:.2f}%".format(name.upper(), proba * 100)
-            box_color = (0, 255*proba, 255*(1-proba))
-            if confidence < conf_threshold:
-                box_color = (0, 0, 255)
-            cv2.rectangle(frame, (x1, y1), (x2, y2), box_color, 2, 8)
-            cv2.rectangle(frame, (x1, int(y1 + (y1-y2)/8)), (x2, y1), box_color, -1, 8)
-            cv2.putText(frame, text, (int(x1 + (x2-x1)/40), int(y1 + (y1-y2)/40)), font, (y2-y1)/420., (255,255,255), 1)
-            new_rectangles.append((x1,y1,x2,y2,name,proba))
-
-    rectangles = new_rectangles[:]
-    return frame
-
-def recognize():
-    """Detects faces present in the video source and saves the video to file"""
-    data = load_database()
+def record(name):
+    """Saves the video extracted from the video source to file at @database_path, then used to learn the face"""
 
     source = 0
     #By default we use 0 but we never know if there's any camera added to device, use it
@@ -193,23 +121,33 @@ def recognize():
     print("[INFO] started camera...")
 
     cap = cv2.VideoCapture(source)
+    has_frame, frame = cap.read()
+
+    vid_writer = cv2.VideoWriter('face.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 15, (frame.shape[1], frame.shape[0]))
 
     frame_count = 0
+    t = time.time()
     tt = 0
+    recording = False
     while True:
         has_frame, frame = cap.read()
         if not has_frame:
             break
         frame_count += 1
 
-        t = time.time()
-        out_frame = process(frame, data, frame_count)
+        out_frame = frame.copy()
         tt += time.time() - t
         fps = frame_count / tt
-        label = "FPS : {:.2f}".format(fps)
-        cv2.putText(out_frame, label, (5, 20), cv2.FONT_HERSHEY_SIMPLEX, .4, (255, 255, 255), 1)
+        t = time.time()
+        if not recording:
+            label = "Press spacebar to start recording   FPS : {:.2f}".format(fps)
+            cv2.putText(out_frame, label, (5, 20), cv2.FONT_HERSHEY_SIMPLEX, .4, (0, 0, 255), 1)
+        else:
+            label = "Press spacebar to stop recording   FPS : {:.2f}".format(fps)
+            cv2.putText(out_frame, label, (5, 20), cv2.FONT_HERSHEY_SIMPLEX, .4, (0, 255, 0), 1)
+            vid_writer.write(out_frame)
 
-        cv2.imshow("Face detection using TensorFlow", out_frame)
+        cv2.imshow("Recording face", out_frame)
 
         if frame_count == 1:
             tt = 0
@@ -217,8 +155,15 @@ def recognize():
         k = cv2.waitKey(10)
         if k == 27:
             break
+        elif k == 32:
+            if recording:
+                break
+            else:
+                recording = True
     cv2.destroyAllWindows()
+    vid_writer.release()
 
-learn()
+    os.mkdir(database_path + "{}\\".format(name))
+    os.rename("face.avi", database_path + "{}\\face.avi".format(name))
 
-serialize()
+record("Romain")
