5b1331ae6bc5d3c7ff1b070e64d8974825b60bc5
Romain Milon <romain.milon@student-cs.fr>
2018-11-20 10:49:45 +0100

    Improved detector interface

--- app/detectors/detector.py
+++ app/detectors/detector.py
@@ -7,20 +7,20 @@ import sys
 import time
 import cv2
 
-frame_process_size = [(192, 108), (256, 144), (320, 180), (300, 300), (426, 240), (640, 360), (1280, 720)][3]
+frame_process_size = [(192, 108), (256, 144), (320, 180), (300, 300), (426, 240), (640, 360), (1280, 720)][4]
 net_models = [("models\\deploy.prototxt","models\\res10_300x300_ssd_iter_140000.caffemodel")]
 font = cv2.FONT_HERSHEY_DUPLEX
 FACE_DETECTION = 0
 
 class Detector():
 
-    def __init__(self, conf_threshold, method, source=-1):
+    def __init__(self, conf_threshold, method, path_to_models="", source=-1):
         """ Method corresponds to the detection method used"""
 
         self.method = method
         self.conf_threshold = conf_threshold
         proto,model = net_models[self.method]
-        self.net = cv2.dnn.readNetFromCaffe(proto,model)
+        self.net = cv2.dnn.readNetFromCaffe(path_to_models + proto, path_to_models + model)
 
         # by default we use 0 but we never know if there's any camera added to device, use it
         if source == -1 and len(sys.argv) > 1:
--- app/detectors/face_detector.py
+++ app/detectors/face_detector.py
@@ -4,7 +4,7 @@ This files contains examples and tests of the detectors
 import cv2
 import app.detectors.detector as dt
 
-detector = dt.Detector(.6, dt.FACE_DETECTION)
+detector = dt.Detector(.9, dt.FACE_DETECTION)
 
 while True:
     detector.next_frame(data_on_frame=True, show_frame=True)
