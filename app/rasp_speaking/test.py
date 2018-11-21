345ad8a73142432ac9a2c6319cc9a49329c4bd07
Briffod Alexis <alexis.briffod@student-cs.fr>
2018-11-21 10:01:13 +0100

    Relative_Path

--- app/rasp_speaking/test.py
+++ app/rasp_speaking/test.py
@@ -1,14 +1,9 @@
-from tkinter import *
-from tkinter.messagebox import *
+
 import os
 
 # Synthese vocale du texte saisi
-def reneParle():
-    os.system('./test.sh &quot;' + lblTexte.get() + '&quot;')
+def reneParle(text):
+    os.system('./app/rasp_speaking/test.sh &quot;' + text + '&quot;')
 
-fen1=Tk()
-lblTexte=Entry(fen1)
-lblTexte.grid(row=3,column=1,padx=5,pady=5)
-btnParle=Button(fen1,text=&quot;Rene, parle !&quot;, command=reneparleParle)
-btnParle.grid(row=3,column=2,padx=5,pady=5)
-fen1.mainloop()
+
+reneParle('Bonjour')
--- app/rasp_speaking/test.sh
+++ app/rasp_speaking/test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Génère un fichier temporaire .wav 
-pico2wave -l fr-FR -w ./temp.wav &quot;$1&quot;
+pico2wave -l fr-FR -w ./app/rasp_speaking/temp.wav &quot;$1&quot;
 # Règle le son 
 amixer sset 'PCM' 95%
 # Joue le son généré
-aplay -q ./temp.wav
+aplay -q ./app/rasp_speaking/temp.wav
 # Suppression du fichier temporaire
-rm ./temp.wav
+rm ./app/rasp_speaking/temp.wav
