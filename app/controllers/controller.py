4c6eb23ebf1ce18e50d89dcffa3750673b172870
Romain Milon <romain.milon@student-cs.fr>
2018-11-21 10:46:00 +0100

    Implemented controllers

--- app/controllers/controller.py
+++ app/controllers/controller.py
@@ -1,14 +1,24 @@
 import cv2
 import app.detectors.detector as dt
+import app.controllers.servo_controller as sct
 
 # define what percent we rotate every servo around each axis per frame
 percent_per_frame = .01
+
+resolution = (1280,720)
 
 class Controller():
 
     def __init__(self, conf_threshold, pins):
         self.rotation = [0,0,0]
-        self.pins = pins
+
+        assert len(pins) == 3
+        sct.setup_GPIO()
+        self.servos = []
+        for i in range(len(pins)):
+            servo = sct.ServoController(pins[i])
+            self.servos.append(servo)
+
         self.conf_threshold = conf_threshold
         self.detector = dt.Detector(conf_threshold, dt.FACE_DETECTION, path_to_models="..\\detectors\\")
 
@@ -23,8 +33,30 @@ class Controller():
         if faces:
             face = faces[0]
             x1,y1,x2,y2,confidence = faces[0]
-            face_center = ((x1+x2)/2, (y1+y2)/2)
-            return face_center
+            x,y = (x1+x2)/2, (y1+y2)/2
+            rotation = self.get_rotation()
+            if x > resolution[0]/2:
+                if rotation[1] <= 1-percent_per_frame:
+                    self.servos[1].set_percent(self.get_rotation()[1]+percent_per_frame)
+                else:
+                    self.servos[1].set_percent(1)
+            else:
+                if rotation[1] >= percent_per_frame:
+                    self.servos[1].set_percent(self.get_rotation()[1]-percent_per_frame)
+                else:
+                    self.servos[1].set_percent(0)
+
+            if y > resolution[1]/2:
+                if rotation[2] <= 1-percent_per_frame:
+                    self.servos[2].set_percent(self.get_rotation()[2]+percent_per_frame)
+                else:
+                    self.servos[2].set_percent(2)
+            else:
+                if rotation[2] >= percent_per_frame:
+                    self.servos[2].set_percent(self.get_rotation()[2]-percent_per_frame)
+                else:
+                    self.servos[2].set_percent(0)
+
 
     def start(self):
         while True:
@@ -33,3 +65,4 @@ class Controller():
                 break
 
         self.detector.close_window()
+        sct.clear()
--- app/controllers/servo_controller_example.py
+++ app/controllers/servo_controller_example.py
@@ -4,7 +4,7 @@ import time
 sct.setup_GPIO()
 controller = sct.ServoController(2)
 time.sleep(1)
-controller.set_percent(100)
+controller.set_percent(1)
 time.sleep(3)
 controller.set_percent(0)
 time.sleep(1)
