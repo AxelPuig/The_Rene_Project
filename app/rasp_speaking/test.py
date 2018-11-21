4afb38de0d56a0474f24bfcea516ad57d8c71ae8
Briffod Alexis <alexis.briffod@student-cs.fr>
2018-11-21 11:03:17 +0100

    Creation et suppression d'un fichier sh

--- app/rasp_speaking/test.py
+++ app/rasp_speaking/test.py
@@ -3,6 +3,19 @@ import os
 
 # Synthese vocale du texte saisi
 def reneParle(text):
-    os.system('sh test.sh &quot;' + text + '&quot;')
+    parole = open("parole.txt", "w")
+    parole.write("""#!/bin/bash
+pico2wave -l fr-FR -w temp.wav '""" + text +
+"""'
+amixer sset 'PCM' 95%
+aplay -q temp.wav
+rm temp.wav""")
+    parole.write("pico2wave -l fr-FR -w temp.wav ")
+    parole.write(text)
+    parole.close()
+    os.rename('parole.txt','parole.sh')
+    os.system('sh test.sh')
+    os.remove('parole.sh')
+
 
 reneParle('Bonjour')
