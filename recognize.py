8a774344fe1b2a878b22b32f07a693162bbecfc4
Briffod Alexis <alexis.briffod@student-cs.fr>
2018-11-22 11:52:59 +0100

    Bonjour_function_if_person_recognized_twice_with_confidence>95

--- recognize.py
+++ recognize.py
@@ -18,16 +18,17 @@ while True:
     frame, data = recognizer.next_frame(data_on_frame=False, show_frame=False)
     print(data)
 
-    if len(data) != 0 and data[0]['confidence_name'] >= 0.95:
-        if data[0]['name'] not in hello_said:  # Si bonjour non dit pour cette personne
-            if data[0]['name'] not in hello_in_process:
-                hello_in_process[data[0]['name']] = [1,
-                                                     0]  # [1 pour le nombre de fois reconnu, 0 à 2 pour les nombre de boucles parcourues depuis la dernière reconnaissance de data[name], au plus 2
-            elif hello_in_process[data[0]['name']][0] == 1:
-                talk.rene_parle('Bonjour ' + data[0]['name'])
-                hello_said.append(data[0]['name'])
-            else:
-                hello_in_process[data[0]['name']] = [1, 0]
+    for person in data:
+        if person['confidence_name'] >= 0.95:
+            if person['name'] not in hello_said:  # Si bonjour non dit pour cette personne
+                if person['name'] not in hello_in_process:
+                    hello_in_process[person['name']] = [1,
+                                                         0]  # [1 pour le nombre de fois reconnu, 0 à 2 pour les nombre de boucles parcourues depuis la dernière reconnaissance de data[name], au plus 2
+                elif hello_in_process[person['name']][0] == 1:
+                    talk.rene_parle('Bonjour ' + person['name'])
+                    hello_said.append(person['name'])
+                else:
+                    hello_in_process[person['name']] = [1, 0]
     for i in hello_in_process:
         if i not in hello_said and hello_in_process[i][0] != 0 :  # pour éviter du travail inutile.
             if hello_in_process[i][1] <= 1:
