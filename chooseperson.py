dd3a42de9bbce6263279cc56e592b79f390465f1
2018puiga <axel.puig@student-cs.fr>
2018-11-22 15:32:52 +0100

    Choice of the person to look at

---
+++ chooseperson.py
@@ -0,0 +1,8 @@
+class ChoosePerson():
+
+    def __init__(self):
+        """ Write something if we need to remember things like the last person followed or something like that """
+        pass
+
+    def choose(self, people):
+        return max(people, key=lambda person: person['confidence_name'])
