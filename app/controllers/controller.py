359ede757f8c7aad97f374e1e1b890b4fe0f0b12
2018puiga <axel.puig@student-cs.fr>
2018-11-22 15:10:24 +0100

    Controller impproved to be used with main

--- app/controllers/controller.py
+++ app/controllers/controller.py
@@ -5,10 +5,12 @@ import app.controllers.servo_controller as sct
 # define what percent we rotate every servo around each axis per frame
 percent_per_frame = .01
 
+
 class Controller():
 
-    def __init__(self, conf_threshold, pins):
+    def __init__(self, pins, conf_threshold=.9, auto_capture=True):
         assert len(pins) == 3
+
         sct.setup_GPIO()
         self.servos = []
         for i in range(len(pins)):
@@ -17,27 +19,43 @@ class Controller():
                 self.servos.append(servo)
 
         self.conf_threshold = conf_threshold
-        self.detector = dt.Detector(conf_threshold, dt.FACE_DETECTION)
+
+        if auto_capture:
+            self.detector = dt.Detector(conf_threshold, dt.FACE_DETECTION)
 
     def next_move(self):
         out_frame, faces = self.detector.next_frame(data_on_frame=True, show_frame=True)
         if faces:
             face = faces[0]
-            x1,y1,x2,y2,confidence = faces[0]
-            height,width = out_frame.shape[0],out_frame.shape[1]
-            x,y = (x1+x2)/2, (y1+y2)/2
-            if x > width/2:
+            x1, y1, x2, y2, confidence = faces[0]
+            height, width = out_frame.shape[0], out_frame.shape[1]
+            x, y = (x1 + x2) / 2, (y1 + y2) / 2
+            if x > width / 2:
                 self.servos[1].add_ratio(percent_per_frame)
             else:
                 self.servos[1].add_ratio(-percent_per_frame)
 
-            if y > height/2:
+            if y > height / 2:
                 self.servos[2].add_ratio(percent_per_frame)
             else:
                 self.servos[2].add_ratio(-percent_per_frame)
 
+    def move(self, face, frame):
+        if face:
+            x1, y1, x2, y2, confidence = face
+            height, width = frame.shape[0], frame.shape[1]
+            x, y = (x1 + x2) / 2, (y1 + y2) / 2
+            if x > width / 2:
+                self.servos[1].add_ratio(percent_per_frame)
+            else:
+                self.servos[1].add_ratio(-percent_per_frame)
 
-    def start(self):
+            if y > height / 2:
+                self.servos[2].add_ratio(percent_per_frame)
+            else:
+                self.servos[2].add_ratio(-percent_per_frame)
+
+    def start_example(self):
         while True:
             print(self.next_move())
             if cv2.waitKey(1) != -1:
