8b0f31e66b4a13758441499d3b57bc1e8210ca7f
Rengati <remi.deschandol@orange.fr>
2018-11-22 16:21:49 +0100

    Update controller.py

--- app/controllers/controller.py
+++ app/controllers/controller.py
@@ -20,14 +20,13 @@ class Controller():
         self.detector = dt.Detector(conf_threshold, dt.FACE_DETECTION)
 
     def next_move(self):
-        print("angle_y : " + str(int(100 * self.servos[0].ratio)))
         out_frame, faces = self.detector.next_frame(data_on_frame=True, show_frame=True)
         if faces:
             face = faces[0]
             x1,y1,x2,y2,confidence = faces[0]
             height,width = out_frame.shape[0],out_frame.shape[1]
             x,y = (x1+x2)/2, (y1+y2)/2
-            print("%.3f : %.3f"%(x/width,y/height))
+            
             if x > width/2:
                 self.servos[0].add_ratio(percent_per_frame)
             else:
