3f9e576c857c539a9d2313817c1605bbc2bdc065
Romain Milon <romain.milon@student-cs.fr>
2018-11-22 11:03:06 +0100

    Fixed Capture

--- app/capture.py
+++ app/capture.py
@@ -1,27 +1,51 @@
 import platform
 import cv2
-if platform.uname()[1] == 'raspberrypi':
-    import app.rasp_compatibility.camera_utils as cam_utils
+from picamera.array import PiRGBArray
+from picamera import PiCamera
 
 OS_RASPBERRY = 'raspberrypi'
 
+if platform.uname()[1] == OS_RASPBERRY:
+    import app.rasp_compatibility.camera_utils as cam_utils
+
+# sensor modes as detailed in https://picamera.readthedocs.io/en/release-1.13/fov.html#sensor-modes
+sensor_modes = [(1920,1080,30),(3280,2464,15),(3280,2464,15),(1640,1232,40),(1640,922,40),(1280,720,90),(640,480,90)]
+
+RES_HD = 0
+RES_FULL_HD = 1
+RES_1232 = 3
+RES_922 = 4
+RES_720 = 5
+RES_480 = 6
 
 class Capture():
-    def __init__(self, source=0):
 
-        self.os = os.uname()[1]
+    def __init__(self, source=0, sensor_mode=RES_480, frame_rate=0):
+        """initialize the camera and grab a reference to the raw camera capture and return a camera object"""
+        self.os = platform.uname()[1]
 
         if self.os == OS_RASPBERRY:
-            self.cap = cam_utils.camera_init()
+            width, height, max_fps = sensor_modes[sensor_mode]
+            if not frame_rate:
+                frame_rate = int(2*max_fps/3)
+
+            self.cap = PiCamera(sensor_mode=sensor_mode + 1, resolution=(width, height), framerate=frame_rate)
         else:
             self.cap = cv2.VideoCapture(source)
 
     def read(self):
+        """return a frame took by the camera in a numpy array format"""
         if self.os == OS_RASPBERRY:
-            return cam_utils.camera_get_frame_adapted(self.cap)
+            raw_capture = PiRGBArray(self.cam, size=self.cam.resolution)
+            self.cam.capture(raw_capture, format="bgr")
+            frame = raw_capture.array
+            return frame is not None, frame
         else:
             return self.cap.read()
 
+    def get_cap(self):
+        return self.cap
+
     def __del__(self):
-        if self.os == OS_RASPBERRY:
+        if self.os != OS_RASPBERRY:
             self.cap.release()
