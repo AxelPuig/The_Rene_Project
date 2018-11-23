1c196dc74c13a57577b49a5a03e5e98ce99d711c
2018puiga <axel.puig@student-cs.fr>
2018-11-23 11:09:44 +0100

    color chooser

---
+++ app/gesture_detection/settings.py
@@ -0,0 +1,37 @@
+import cv2
+import imutils
+import numpy as np
+
+from app.gesture_detection.color_choosing import Settings
+
+def skin_detector(grabbed,frame): #define a function to blur the "non-skin" pixels
+    frame = imutils.resize(frame, width = 400)
+    converted = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    skinMask = cv2.inRange(converted, lower, upper)
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (11, 11))
+    skinMask = cv2.erode(skinMask, kernel, iterations = 2)
+    skinMask = cv2.dilate(skinMask, kernel, iterations = 2)
+    skinMask = cv2.GaussianBlur(skinMask, (3, 3), 0)
+    skin = cv2.bitwise_and(frame, frame, mask = skinMask)
+    new_frame = np.hstack([frame, skin])
+    return ret, new_frame
+
+
+s = Settings(6)
+cap = cv2.VideoCapture(0)
+
+k = -1
+while k == -1:
+    # get frame
+    v1, v2, v3, v4, v5, v6 = s.get_settings_window_values()
+
+    lower = np.array([v1, v3, v5], dtype = "uint8")       #Define the range of colors that seems to be skin color
+    upper = np.array([v2, v4, v6], dtype = "uint8")
+
+    ret, frame = cap.read()
+
+    _, new_frame = skin_detector(ret,frame)
+
+    k = s.update_settings_window(new_frame)
+
+cv2.destroyAllWindows()
