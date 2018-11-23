a2c84685267aae8df62e3eb8acc89d9a5a4dde10
Romain Milon <romain.milon@student-cs.fr>
2018-11-23 10:41:42 +0100

    Added comments

--- app/rasp_speaking/talk.py
+++ app/rasp_speaking/talk.py
@@ -1,14 +1,13 @@
-
 import os
 import platform
 
-
 def rene_parle(text):
     """Synthese vocale du texte saisi (text), par le Raspberry.
-    Si on est sur l'ordi, ça print le texte"""
+    Si on est sur l'ordi, ça print le texte
+    :param text: text to synthetize"""
 
     if platform.uname()[1] == "raspberrypi":
-        parole = open("parole.txt", "w") #création d'un fichier text
+        parole = open("parole.txt", "w") #création d'un fichier texte
 
         parole.write("""#!/bin/bash
 pico2wave -l fr-FR -w temp.wav '""" + text +
@@ -18,12 +17,11 @@ omxplayer temp.wav
 rm temp.wav""")
     
         parole.close()
-        os.rename('parole.txt','parole.sh') #fichier text --> fichier shell
+        os.rename('parole.txt','parole.sh') #fichier texte --> fichier shell
         os.system('sh parole.sh') #execution du fichier shell
         os.remove('parole.sh') # suppression du fichier shell
 
     else:
         print("[RASPI SAYS] " + text)
-
 
 #rene_parle('Bonjour')
